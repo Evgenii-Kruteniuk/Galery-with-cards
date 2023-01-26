2a955c5a6d3f4114d9c0983021df3cfd16f237f3
Evgenii-Kruteniuk <muster00100@yandex.ru>
2023-01-27 00:12:52 +0300

    27.01.23

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const slides = document.querySelectorAll(".slide");
+/*Создаем функцию slidesPlugin для того, чтобы мы могли
+менять активную картинку. По умолчанию ставим 2-ю картирку
+с классом active т. е. большую*/
+function slidesPlugin(activeSlide = 2) {
+  /*Создаем константу slides и связываем ее с классом slide */
+  const slides = document.querySelectorAll(".slide");
 
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
-    slide.classList.add("active");
-  });
+  /*Создаем массив слайдов в котором при выборе картинки по
+ индексу на ней будет класс active */
+  slides[activeSlide].classList.add("active");
+
+  for (const slide of slides) {
+    //Вешаем событие при клике на слайд
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      //При клике на слайд ему добавляется active
+      slide.classList.add("active");
+    });
+  }
+  /*Создаем функцию которая будет чистить не активный класс*/
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
-}
+slidesPlugin(1);
